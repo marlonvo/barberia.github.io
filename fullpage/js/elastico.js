59fcbdf518993a614710158ea534b2016c9f179c
marlonvo <141679211+marlonvo@users.noreply.github.com>
2023-08-11 20:15:28 -0600

    Add files via upload

---
+++ fullpage/js/elastico.js
@@ -0,0 +1,34 @@
+//Elasticidad botones menu
+$(document).ready(function() {
+    smoothScroll.init({
+        selector: '[data-scroll]',
+        //selector: 'a.nav-link', 
+        // 2 seg equivale a 2000)  
+        speed: 1000,
+        //aceleraciones
+        easing: 'easeInQuart',
+        //easing: 'easyInCubic'
+        //easing: 'easyInOutQuad',
+        //easing:'easeInQuart',
+        //easing: 'easeInOutCubic',
+    });
+
+});
+
+
+//desaparece el boton ScrollTop al cargar la pagina web
+document.getElementById("flechabtn").style.display = "none";
+
+//esconder botón ScrollTop (parte superior)
+window.onscroll = function() {
+    scrollFunction()
+};
+
+//funcion scroll
+function scrollFunction() {
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+        document.getElementById("flechabtn").style.display = "block";
+    } else {
+        document.getElementById("flechabtn").style.display = "none";
+    }
+}
